dd174e036e79fcea7a97c20dfa1cadef73e0afc9
Kunal R <kunal.rustagi@gmail.com>
2020-08-08 11:13:56 +0530

    fix errors

--- annoyed-alligators/socl_media/apps/users/views.py
+++ annoyed-alligators/socl_media/apps/users/views.py
@@ -64,16 +64,16 @@ def profile_edit(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
                                    instance=request.user.profile)
-        
+
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
             messages.success(request, "Account Information Updated!")
-            return redirect('profile', username)
+            return redirect('profile', request.user.username)
 
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile) 
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
